eff16503d588074f3c7aede165e4250eca76fc55
Pedro Wagner <pedrowagner0801@gmail.com>
2024-08-11 16:38:34 -0300

    implemented reader

--- src/application/main.java
+++ src/application/main.java
@@ -1,16 +1,16 @@
 package application;
 
-import java.io.*;
-import java.util.Scanner;
 import controller.Controller;
-import model.Pessoa;
 
 
 public class main {
 
-	public static void main(String[] args) throws IOException {
-		Controller controlador = new Controller(); 
-		
+	public static void main(String[] args) {
+		try {
+			Controller control = new Controller();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
