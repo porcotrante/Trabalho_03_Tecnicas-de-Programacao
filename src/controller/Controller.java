25c0a6560503e70570c0ba5792aedf36e3ec5261
Pedro Militão Bona Lopes <pmbona@outlook.com>
2024-08-12 03:31:53 -0300

    prints moved for Controller method and updatePessoa and deletePessoa methods created

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,86 +3,156 @@ package controller;
 import java.util.Scanner;
 import java.io.*;
 
-
 public class Controller {
 
-	public Controller() throws IOException {
-	Scanner sc = new Scanner(System.in);
-	int repetir = 0;
+	private static String filepath = "../pessoa.txt";
 
-	while (repetir == 0) {
-		System.out.println("Qual operação você deseja realizar?");
-		System.out.println("Digite C para cadastrar uma pessoa,");
-		System.out.println("Digite R para consultar uma pessoa, ");
-		System.out.println("Digite U para atualizar o cadastro de uma pessoa,");
-		System.out.println("Digite D para deletar o cadastro de uma pessoa,");
-		System.out.println("Ou digite S para sair e não realizar nenhuma operação.");
-		String operacao = sc.next(); 
-
-		switch(operacao) {
-			case "C": 
-				createPessoa(sc); 
-				break;
-			case "R":
-				System.out.println("Qual o nome da pessoa que quer consultar?");
-				String nome = sc.next();
-				readPessoa(nome);
-				break;
-			case "U":
-				break;
-			case "D":
-				break;
-			case "S":
-				System.out.println("Operação finalizada, obrigado por utilizar o cadastrador de pessoas.");
-				repetir = 1; 
-				break;
-			default:
-				System.out.println("Comando inválido, tente novamente.");
-				break;
-			}
-			
+	private static String getFilepath() {
+		return filepath;
 	}
 
-	sc.close();
-}
-	
-	public static void createPessoa(Scanner sc) throws IOException {
-		PrintWriter pw = new PrintWriter(new FileWriter("./pessoa.txt", true)); 
+	public Controller() throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int repetir = 0;
 
-		System.out.println("Qual o nome da pessoa?");
-		String nome = sc.next(); 
-		System.out.println("Qual a idade da pessoa?");
-		String idade = sc.next();
-		System.out.println("Qual o telefone da pessoa?");
-		String telefone = sc.next();
+		while (repetir == 0) {
+			System.out.println("Qual operação você deseja realizar?");
+			System.out.println("Digite C para cadastrar uma pessoa,");
+			System.out.println("Digite R para consultar uma pessoa, ");
+			System.out.println("Digite U para atualizar o cadastro de uma pessoa,");
+			System.out.println("Digite D para deletar o cadastro de uma pessoa,");
+			System.out.println("Ou digite S para sair e não realizar nenhuma operação.");
+			String operacao = sc.next();
 
-		pw.print(nome+";");
-		pw.print(idade+";");
-		pw.print(telefone+"\n");
+			String nome, idade, telefone;
+			switch (operacao) {
+				case "C":
+					System.out.println("Qual o nome da pessoa?");
+					nome = sc.next();
+					System.out.println("Qual a idade da pessoa?");
+					idade = sc.next();
+					System.out.println("Qual o telefone da pessoa?");
+					telefone = sc.next();
+					if (createPessoa(nome, idade, telefone)) {
+						System.out.println("Cadastro feito com sucesso!");
+					} else {
+						System.out.println("Essa pessoa já foi cadastrada!\nOperação cancelada");
+					}
+					break;
+				case "R":
+					System.out.println("Qual o nome da pessoa que quer consultar?");
+					nome = sc.next();
+					String[] dados = readPessoa(nome);
+					if (dados != null) {
+						System.out.println("Pessoa encontrada!");
+						System.out.println("Nome: " + dados[0]);
+						System.out.println("Idade: " + dados[1]);
+						System.out.println("Telefone: " + dados[2]);
+					} else {
+						System.out.println("Esse nome não está em nossos registros!");
+					}
+					break;
+				case "U":
+					System.out.println("Qual o nome da pessoa cujos dados serão alterados?");
+					nome = sc.next();
+					System.out.println("Qual a idade da pessoa?");
+					idade = sc.next();
+					System.out.println("Qual o telefone da pessoa?");
+					telefone = sc.next();
+					if (updatePessoa(nome, idade, telefone)) {
+						System.out.println("Atualização realizada com sucesso");
+					} else {
+						System.out.println("Esse nome não está em nossos registros!");
+					}
+					break;
+				case "D":
+					System.out.println("Qual o nome da pessoa cujos dados serão deletados?");
+					nome = sc.next();
+					if (deletePessoa(nome)) {
+						System.out.println("Deleção executada com sucesso");
+					} else {
+						System.out.println("Esse nome não está em nossos registros!");
+					}
+					break;
+				case "S":
+					System.out.println("Operação finalizada, obrigado por utilizar o cadastrador de pessoas.");
+					repetir = 1;
+					break;
+				default:
+					System.out.println("Comando inválido, tente novamente.");
+					break;
+			}
 
-		System.out.println("Cadastro feito com sucesso!");
+		}
+
+		sc.close();
+	}
+
+	public static boolean createPessoa(String nome, String idade, String telefone) throws IOException {
+		PrintWriter pw = new PrintWriter(new FileWriter(getFilepath(), true));
+		boolean criada = false;
+		if (readPessoa(nome) == null) {
+			pw.print(nome + ";");
+			pw.print(idade + ";");
+			pw.print(telefone + "\n");
+			criada = true;
+		}
 		pw.flush();
 		pw.close();
+		return criada;
 	}
 
-	public static void readPessoa(String nome) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("./pessoa.txt")));
+	public static String[] readPessoa(String nome) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(getFilepath())));
 		String linha;
+		boolean encontrado = false;
+		String[] dados = null;
 		while ((linha = br.readLine()) != null) {
-			String[] dados = linha.split(";");
-
+			dados = linha.split(";");
 			if (dados[0].equals(nome)) {
-				System.out.println("Pessoa encontrada!");
-				System.out.println("Nome: "+dados[0]);
-				System.out.println("Idade: "+dados[1]);
-				System.out.println("Telefone: "+dados[2]);
+				encontrado = true;
 				break;
 			}
-
-		}
-		if (linha == null) {
-			System.out.println("Pessoa não encontrada");
 		}
 		br.close();
+		if (encontrado) {
+			return dados;
+		} else {
+			return null;
+		}
+	}
+
+	private static boolean updatePessoa(String nome, String idade, String telefone) throws IOException {
+		boolean atualizado = false;
+		if (deletePessoa(nome)) {
+			createPessoa(nome, idade, telefone);
+			atualizado = true;
+		}
+		return atualizado;
+	}
+
+	private static boolean deletePessoa(String nome) throws IOException {
+		File oldFile = new File(getFilepath());
+		File newFile = new File("../temp.txt");
+		BufferedReader br = new BufferedReader(new FileReader(oldFile));
+		PrintWriter pw = new PrintWriter(new FileWriter(newFile, true));
+		boolean deletado = false;
+
+		String linha;
+		String[] dados;
+		while ((linha = br.readLine()) != null) {
+			dados = linha.split(";");
+			if (!(dados[0].equals(nome))) {
+				pw.print(linha + "\n");
+			} else {
+				deletado = true;
+			}
+		}
+		br.close();
+		pw.close();
+
+		oldFile.delete();
+		newFile.renameTo(new File(getFilepath()));
+		return deletado;
 	}
 }
